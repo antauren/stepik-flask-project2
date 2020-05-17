5bb45836a5afd251658279bad7ab416b5b059e07
Anatoly <antauren@yandex.ru>
2020-05-17 09:38:52 +0300

    step
    1. Распишите роуты, выведите текст:
    
        - главной / – здесь будет главная),
    
        - цели /goals/<goal>/  – здесь будет цель <goal>,
        - профиля учителя /profiles/<id учителя>/ – здесь будет преподаватель <id учителя>,
    
        - заявки на подбор /request/ – здесь будет заявка на подбор,
        - принятой заявки на подбор /request_done/ – заявка на подбор отправлена,
    
        - формы бронирования /booking/<id учителя>/<день недели>/<время>/ – здесь будет форма бронирования <id учителя>,
        - принятой заявки на подбор /booking_done/   – заявка отправлена.

--- app.py
+++ app.py
@@ -4,8 +4,38 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def render_index():
+    return 'здесь будет главная'
+
+
+@app.route('/goals/<goal>/')
+def render_goal(goal):
+    return 'здесь будет цель {}'.format(goal)
+
+
+@app.route('/profiles/<profile_id>/')
+def render_profile(profile_id):
+    return 'здесь будет преподаватель <id учителя> {}'.format(profile_id)
+
+
+@app.route('/request/')
+def render_request():
+    return 'здесь будет заявка на подбор'
+
+
+@app.route('/request_done/')
+def render_request_done():
+    return 'заявка на подбор отправлена'
+
+
+@app.route('/booking/<profile_id>/<day>/<time>/')
+def render_booking(profile_id, day, time):
+    return 'здесь будет форма бронирования <id учителя>'.format(profile_id, day, time)
+
+
+@app.route('/booking_done/')
+def render_booking_done():
+    return 'заявка отправлена'
 
 
 if __name__ == '__main__':
