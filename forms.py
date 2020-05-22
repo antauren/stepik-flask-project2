ee859eee28202f61c7689fcb58d58c82775c9cf0
Anatoly <antauren@yandex.ru>
2020-05-22 23:01:42 +0300

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields.html5 import TelField
+from wtforms.validators import InputRequired
+
+
+class ClientForm(FlaskForm):
+    name = StringField('Вас зовут', [InputRequired(message='Это поле не может быть пустым.')])
+    phone = TelField('Ваш телефон', [InputRequired(message='Это поле не может быть пустым.')])
