878ad85c6aefca6378d4a7abca4b450276a0e4e2
Anatoly <antauren@yandex.ru>
2020-05-23 12:48:40 +0300

    Revert "use encoding"
    This reverts commit e3f11282beee2601f88417256ee163d2aacdefda.

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import json
 
 
-def write_json_file(path, data, indent=4, encoding='utf-8'):
-    with open(path, 'w', encoding=encoding) as fd:
-        json.dump(data, fd, indent=indent, ensure_ascii=False)
+def write_json_file(path, data, indent=4):
+    with open(path, 'w') as fd:
+        json.dump(data, fd, indent=indent)
 
 
-def read_json_file(path, encoding='utf-8'):
-    with open(path, 'r', encoding=encoding) as fd:
+def read_json_file(path):
+    with open(path, 'r') as fd:
         return json.load(fd)
