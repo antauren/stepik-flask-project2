f825d43a0617a0db52b64f5820f04d8b21825fbd
Anatoly <antauren@yandex.ru>
2020-05-23 04:16:20 +0300

    use SubmitField

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.fields.html5 import TelField
 from wtforms.validators import InputRequired
 
@@ -7,3 +7,4 @@ from wtforms.validators import InputRequired
 class ClientForm(FlaskForm):
     name = StringField('Вас зовут', [InputRequired(message='Это поле не может быть пустым.')])
     phone = TelField('Ваш телефон', [InputRequired(message='Это поле не может быть пустым.')])
+    submit = SubmitField()
