9f48d8bf9335b69e205fda9d2ab65575329f48d8
Anatoly <antauren@yandex.ru>
2020-05-23 18:20:22 +0300

    add BookingForm

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, HiddenField
 from wtforms.fields.html5 import TelField
 from wtforms.validators import InputRequired
 
@@ -8,3 +8,9 @@ class ClientForm(FlaskForm):
     name = StringField('Вас зовут', [InputRequired(message='Это поле не может быть пустым.')])
     phone = TelField('Ваш телефон', [InputRequired(message='Это поле не может быть пустым.')])
     submit = SubmitField()
+
+
+class BookingForm(ClientForm):
+    day = HiddenField()
+    time = HiddenField()
+    profile_id = HiddenField()
